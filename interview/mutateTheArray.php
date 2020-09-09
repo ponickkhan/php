472023d3fed0f805b576dafb2cb033bf58ea1a02
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-09-09 18:43:06 +0600

    git init

---
+++ interview/mutateTheArray.php
@@ -0,0 +1,23 @@
+<?php
+
+function mutateTheArray($n,$a){
+$b = [];
+
+for($i=0; $i<count($a); $i++) {
+
+    print($a[$i])."-";
+
+}
+
+
+}
+
+$n = 7;
+
+$a = [4,0,1,-2,3];
+
+
+mutateTheArray($n,$a);
+
+
+
